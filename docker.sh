c7c5c6f1ca8ccae2b14fe4a526e1fe29dd7fa7bb
Daniel Henrique <queirozdaniel1999@gmail.com>
2024-02-29 00:41:56 -0300

    o

--- docker.sh
+++ docker.sh
@@ -3,3 +3,7 @@ curl -fsSL https://get.docker.com | sudo bash
 sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker vagrant
+
+sudo mkdir pasta
+sudo touch pasta/arquivo.txt
+sudo docker-compose up -d
--- master.sh
+++ master.sh
@@ -3,5 +3,5 @@ sudo docker swarm init --advertise-addr=10.10.10.100
 sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
 
 
-
+docker volume create storage_volume
 
